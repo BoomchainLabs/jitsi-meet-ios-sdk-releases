d5a62350e591d052cd97a6032145fdbc8bfd4bb2
Calinteodor <calinte2002@yahoo.co.uk>
2026-07-29 15:08:47 +0300

    Fix SPM binary target: hermes -> hermesvm

--- lite/Package.swift
+++ lite/Package.swift
@@ -19,14 +19,14 @@ let package = Package(
             path: "../lite/Frameworks/JitsiMeetSDK.xcframework"
         ),
         .binaryTarget(
-            name: "hermes",
-            path: "../lite/Frameworks/hermes.xcframework"
+            name: "hermesvm",
+            path: "../lite/Frameworks/hermesvm.xcframework"
         ),
         .target(
             name: "JitsiMeetSDKWrapper",
             dependencies: [
                 .target(name: "JitsiMeetSDK"),
-                .target(name: "hermes"),
+                .target(name: "hermesvm"),
                 .product(name: "WebRTC", package: "webrtc")
             ],
             path: "Sources"
